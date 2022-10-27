73754d2d2d9ffbdef8291bd758743e1806955109
Marcin Stasiak <kontakt@marcinstasiak.pl>
2022-10-27 07:06:39 +0200

    ad folder for sql cache

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  name: process.env.DATABASE_NAME || 'ussr',
+  name: process.env.DATABASE_NAME || 'cache/pages.db',
   // username: process.env.DATABASE_USERNAME || '',
   // password: process.env.DATABASE_PASSWORD || '',
 }));
